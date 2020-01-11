74c77c8d2b395d100dd670dc459eeb1fe3021ee3
Lambis <lambiselef@gmail.com>
2020-01-11 19:59:52 +0200

    Dissociate threads from block size.

--- ising_cuda_v3.cu
+++ ising_cuda_v3.cu
@@ -10,7 +10,8 @@
 #include <time.h>
 
 #define N 512
-#define threadsNum 64
+#define threadsNum 128
+#define blockSize 32
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
@@ -25,10 +26,10 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 // Kernel Function.
 __global__ void spin(int *G_d, double *w, int *newG_d, int n) {
 
-    const int G_length = threadsNum + 4;  // = blockDim.x+4;
+    const int G_length = blockSize + 4;  // = blockDim.x+4;
 
     // Calculates the first Atomic Spin index. Note: n/blockDim.x=sqrt(gridDim.x).
-    int index = (blockIdx.x/(n/blockDim.x))*n*blockDim.x + (blockIdx.x%(n/blockDim.x))*blockDim.x;
+    int index = (blockIdx.x/(n/blockSize))*n*blockSize + (blockIdx.x%(n/blockSize))*blockSize;
 
     // Checks for out of bounds indexing and if so quits.
     if (index >= n*n)
@@ -36,45 +37,46 @@ __global__ void spin(int *G_d, double *w, int *newG_d, int n) {
 
     // Block-exclusive Atomic Spins Matrix G[G_length^2] is transfered to shared memory.
     __shared__ int G[G_length*G_length];
+    //for (int i=threadIdx.x; i<G_length*G_length; i+=blockDim.x)
     for (int i=threadIdx.x*(G_length*G_length/blockDim.x + 1); i<(threadIdx.x+1)*(G_length*G_length/blockDim.x + 1) && i<G_length*G_length; i++)
-        G[i] = G_d[((index/n - 2 + i/G_length + n) % n) * n + (index%n - 2 + i%G_length + n) % n];
+    
+	G[i] = G_d[((index/n - 2 + i/G_length + n) % n) * n + (index%n - 2 + i%G_length + n) % n];
     __syncthreads();
 
-    index += threadIdx.x;
     double weightSum;
-    for (int i=index; i<index+blockDim.x*n; i+=n) {
+    for (int i=index+threadIdx.x/blockSize*n+threadIdx.x%blockSize; i<(index+(blockSize-1)*n+blockSize) && i<n*n; i+=((i%n)%blockSize+blockDim.x)/blockSize*n + ((i%n)%blockSize+blockDim.x)%blockSize - (i%n)%blockSize) {
         weightSum = 0;
 
         // Calculates weight contribution for each neighboring Atomic Spin and sums it.
-        weightSum += w[0] * G[((i/n)%blockDim.x + 2 - 2)*G_length +(i%n)%blockDim.x + 2 - 2];
-        weightSum += w[1] * G[((i/n)%blockDim.x + 2 - 2)*G_length +(i%n)%blockDim.x + 2 - 1];
-        weightSum += w[2] * G[((i/n)%blockDim.x + 2 - 2)*G_length +(i%n)%blockDim.x + 2];
-        weightSum += w[3] * G[((i/n)%blockDim.x + 2 - 2)*G_length +(i%n)%blockDim.x + 2 + 1];
-        weightSum += w[4] * G[((i/n)%blockDim.x + 2 - 2)*G_length +(i%n)%blockDim.x + 2 + 2];
+        weightSum += w[0] * G[((i/n)%blockSize + 2 - 2)*G_length +(i%n)%blockSize + 2 - 2];
+        weightSum += w[1] * G[((i/n)%blockSize + 2 - 2)*G_length +(i%n)%blockSize + 2 - 1];
+        weightSum += w[2] * G[((i/n)%blockSize + 2 - 2)*G_length +(i%n)%blockSize + 2];
+        weightSum += w[3] * G[((i/n)%blockSize + 2 - 2)*G_length +(i%n)%blockSize + 2 + 1];
+        weightSum += w[4] * G[((i/n)%blockSize + 2 - 2)*G_length +(i%n)%blockSize + 2 + 2];
 
-        weightSum += w[5] * G[((i/n)%blockDim.x + 2 - 1)*G_length +(i%n)%blockDim.x + 2 - 2];
-        weightSum += w[6] * G[((i/n)%blockDim.x + 2 - 1)*G_length +(i%n)%blockDim.x + 2 - 1];
-        weightSum += w[7] * G[((i/n)%blockDim.x + 2 - 1)*G_length +(i%n)%blockDim.x + 2];
-        weightSum += w[8] * G[((i/n)%blockDim.x + 2 - 1)*G_length +(i%n)%blockDim.x + 2 + 1];
-        weightSum += w[9] * G[((i/n)%blockDim.x + 2 - 1)*G_length +(i%n)%blockDim.x + 2 + 2];
+        weightSum += w[5] * G[((i/n)%blockSize + 2 - 1)*G_length +(i%n)%blockSize + 2 - 2];
+        weightSum += w[6] * G[((i/n)%blockSize + 2 - 1)*G_length +(i%n)%blockSize + 2 - 1];
+        weightSum += w[7] * G[((i/n)%blockSize + 2 - 1)*G_length +(i%n)%blockSize + 2];
+        weightSum += w[8] * G[((i/n)%blockSize + 2 - 1)*G_length +(i%n)%blockSize + 2 + 1];
+        weightSum += w[9] * G[((i/n)%blockSize + 2 - 1)*G_length +(i%n)%blockSize + 2 + 2];
 
-        weightSum += w[10] * G[((i/n)%blockDim.x + 2)*G_length +(i%n)%blockDim.x + 2 - 2];
-        weightSum += w[11] * G[((i/n)%blockDim.x + 2)*G_length +(i%n)%blockDim.x + 2 - 1];
+        weightSum += w[10] * G[((i/n)%blockSize + 2)*G_length +(i%n)%blockSize + 2 - 2];
+        weightSum += w[11] * G[((i/n)%blockSize + 2)*G_length +(i%n)%blockSize + 2 - 1];
         // w[12] is not contributing anything. It's the current Atomic Spin.
-        weightSum += w[13] * G[((i/n)%blockDim.x + 2)*G_length +(i%n)%blockDim.x + 2 + 1];
-        weightSum += w[14] * G[((i/n)%blockDim.x + 2)*G_length +(i%n)%blockDim.x + 2 + 2];
+        weightSum += w[13] * G[((i/n)%blockSize + 2)*G_length +(i%n)%blockSize + 2 + 1];
+        weightSum += w[14] * G[((i/n)%blockSize + 2)*G_length +(i%n)%blockSize + 2 + 2];
 
-        weightSum += w[15] * G[((i/n)%blockDim.x + 2 + 1)*G_length +(i%n)%blockDim.x + 2 - 2];
-        weightSum += w[16] * G[((i/n)%blockDim.x + 2 + 1)*G_length +(i%n)%blockDim.x + 2 - 1];
-        weightSum += w[17] * G[((i/n)%blockDim.x + 2 + 1)*G_length +(i%n)%blockDim.x + 2];
-        weightSum += w[18] * G[((i/n)%blockDim.x + 2 + 1)*G_length +(i%n)%blockDim.x + 2 + 1];
-        weightSum += w[19] * G[((i/n)%blockDim.x + 2 + 1)*G_length +(i%n)%blockDim.x + 2 + 2];
+        weightSum += w[15] * G[((i/n)%blockSize + 2 + 1)*G_length +(i%n)%blockSize + 2 - 2];
+        weightSum += w[16] * G[((i/n)%blockSize + 2 + 1)*G_length +(i%n)%blockSize + 2 - 1];
+        weightSum += w[17] * G[((i/n)%blockSize + 2 + 1)*G_length +(i%n)%blockSize + 2];
+        weightSum += w[18] * G[((i/n)%blockSize + 2 + 1)*G_length +(i%n)%blockSize + 2 + 1];
+        weightSum += w[19] * G[((i/n)%blockSize + 2 + 1)*G_length +(i%n)%blockSize + 2 + 2];
 
-        weightSum += w[20] * G[((i/n)%blockDim.x + 2 + 2)*G_length +(i%n)%blockDim.x + 2 - 2];
-        weightSum += w[21] * G[((i/n)%blockDim.x + 2 + 2)*G_length +(i%n)%blockDim.x + 2 - 1];
-        weightSum += w[22] * G[((i/n)%blockDim.x + 2 + 2)*G_length +(i%n)%blockDim.x + 2];
-        weightSum += w[23] * G[((i/n)%blockDim.x + 2 + 2)*G_length +(i%n)%blockDim.x + 2 + 1];
-        weightSum += w[24] * G[((i/n)%blockDim.x + 2 + 2)*G_length +(i%n)%blockDim.x + 2 + 2];
+        weightSum += w[20] * G[((i/n)%blockSize + 2 + 2)*G_length +(i%n)%blockSize + 2 - 2];
+        weightSum += w[21] * G[((i/n)%blockSize + 2 + 2)*G_length +(i%n)%blockSize + 2 - 1];
+        weightSum += w[22] * G[((i/n)%blockSize + 2 + 2)*G_length +(i%n)%blockSize + 2];
+        weightSum += w[23] * G[((i/n)%blockSize + 2 + 2)*G_length +(i%n)%blockSize + 2 + 1];
+        weightSum += w[24] * G[((i/n)%blockSize + 2 + 2)*G_length +(i%n)%blockSize + 2 + 2];
 
         //! Can it be done more efficiently?
         if (weightSum > 0.0001)
@@ -91,13 +93,13 @@ __global__ void spin(int *G_d, double *w, int *newG_d, int n) {
 __global__ void check(int *G, int *newG, int n, int *same) {
 
     // Calculates Atomic Spin index.
-    int index = (blockIdx.x/(n/blockDim.x))*n*blockDim.x + (blockIdx.x%(n/blockDim.x))*blockDim.x + threadIdx.x;;
+    int index = (blockIdx.x/(n/blockDim.x))*n*blockDim.x + (blockIdx.x%(n/blockDim.x))*blockDim.x;
 
     // Checks for out of bounds indexing and if so quits.
     if (index >= n*n)
         return;
     
-    for (int i=index; i<index+blockDim.x*n; i+=n)
+    for (int i=index+threadIdx.x/blockSize*n+threadIdx.x%blockSize; i<(index+(blockSize-1)*n+blockSize) && i<n*n; i+=((i%n)%blockSize+threadsNum)/blockSize*n + ((i%n)%blockSize+threadsNum)%blockSize - (i%n)%blockSize)
         if (G[index] != newG[index]) {
 	    *same = 0;
 	    break;
@@ -133,21 +135,20 @@ void ising(int *G, double *w, int k, int n) {
     int *temp_d;
 
     // Checks if function has to be iterated.
-    while (k != 0) {
+    for (int i=0; i<k; i++) {
         // Calls the kernel function balancing load to (n/threadsNum)^2 blocks with threadsNum threads each.
         // Each thread calculates threadsNum spins.
         //! User has to specify numbers fitting the data correctly (sqrt(gridDim) * blockDim = n).
-        spin<<<n/threadsNum*n/threadsNum,threadsNum>>>(G_d, w_d, newG_d, n);
+        spin<<<n/blockSize*n/blockSize,threadsNum>>>(G_d, w_d, newG_d, n);
         gpuErrchk( cudaPeekAtLastError() );
         gpuErrchk( cudaDeviceSynchronize() );
-        k--;
 
-	check<<<n/threadsNum*n/threadsNum,threadsNum>>>(G_d, newG_d, n, same_d);
-        gpuErrchk( cudaPeekAtLastError() );
-        gpuErrchk( cudaDeviceSynchronize() );
-	gpuErrchk( cudaMemcpy(&same, same_d, sizeof(int), cudaMemcpyDeviceToHost) );
-	if (same)
-	    break;
+	//check<<<n/blockSize*n/blockSize,threadsNum>>>(G_d, newG_d, n, same_d);
+        //gpuErrchk( cudaPeekAtLastError() );
+        //gpuErrchk( cudaDeviceSynchronize() );
+	//gpuErrchk( cudaMemcpy(&same, same_d, sizeof(int), cudaMemcpyDeviceToHost) );
+	//if (same)
+	//    break;
 
         // Atomix Spin Matrices' pointers swapping.
         temp_d = G_d;
@@ -203,7 +204,7 @@ int main() {
         G[i] = initG[i];
     */
 
-    ising(G, w, 1000, n);
+    ising(G, w, 10, n);
 
     /*
     // Reads configuration file for state after one iteration.
